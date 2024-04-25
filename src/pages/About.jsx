54b8387137eb69503e8fc1c21913772b60020f9a
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2024-04-25 22:43:43 +0530

    now complete the page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,36 @@ import React from "react";
 const About = () => {
     return (
         <div className="about">
-            hello
+        <div className="heading">
+            <h1>ABOUT ME</h1> 
+
+        </div>
+        <div className="Description">
+            <p>Hello, I'm Sanjeev Varma, a Computer Science student, blockchain developer, and researcher from India. My primary focus is on creating well-developed blockchain software and conducting high-quality research in this field.
+
+Currently, I am seeking like-minded individuals to collaborate with who share an interest in blockchain technology and its potential applications.
+
+
+
+
+
+
+
+            </p>
+        
+        </div>
+        <div className="interest card">
+            my interests
+        </div>
+        <div className="tech-stack-card">
+            tech stack i use
+        </div>
+        <div className="research-area-card">
+            use this research card
+        </div>
+        <div className=" currenlty working on">
+
+        </div>
         </div>
     )
 };
